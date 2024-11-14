8ef6c86a70b137201d9ad42e516efa3ebc3f4138
atamayev <75034340+actamayev@users.noreply.github.com>
2024-11-14 13:05:40 -0500

    take user code as string instead of file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ USER_CODE_FILE="$WORKSPACE_DIR/src/user_code.cpp"
 # Ensure the src directory exists
 mkdir -p "$WORKSPACE_DIR/src"
 
-# Write user code to `user_code.cpp` in the src directory
-echo "$1" > "$USER_CODE_FILE"
+# Write the user code from the environment variable to `user_code.cpp`
+echo -e "$USER_CODE" > "$USER_CODE_FILE"
 
 # Move to the workspace directory and compile
 cd "$WORKSPACE_DIR"
 platformio run
 
-# Print the location of the compiled binary
+# Print the location of the compiled firmware binary
 echo "Firmware compiled successfully! Check the .pio/build directory for the binary."
