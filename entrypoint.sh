27fbdfb96a6bf09caddac520757c48167961cb41
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-12 22:49:55 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,8 +59,10 @@ fi
 # Create new user code file in workspace
 log "Creating user code file in workspace..."
 cat > "$USER_CODE_FILE" << EOL
+#include "./include/utils.h"
 #include "./include/config.h"
 #include "./include/rgb_led.h"
+#include "./include/sensors.h"
 #include "./include/user_code.h"
 
 void user_code() {
